6b9d294ac10e8ffbdd1c96df715bdafc2e817b36
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-03-23 18:07:24 +0800

    Initial example.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img
+    ref="logo"
+    class="logo"
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+
+  <div>
+    <button @click="child.sayHi('template')">Say Hi</button>
+  </div>
+
+  <Child
+    ref="child"
+    msg="Welcome to Your Vue.js + TypeScript App"
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, nextTick, onMounted, ref } from 'vue';
+import Child from './components/Child.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Child
+  },
+  setup () {
+    // DOM
+    const logo = ref<HTMLImageElement | null>(null);
+
+    // 子组件，使用 typeof 类型保护
+    const child = ref<typeof Child | null>(null);
+
+    onMounted( () => {
+      /** 
+       * 可选方式：通过 ? 来将目标设置为可选，避免节点未挂载成功时出现错误
+       * 这个方式不能修改子组件的值
+       */
+      console.log(logo.value?.getAttribute('src'));
+      console.log(child.value?.isShow);
+      child.value?.sayHi('use ? in onMounted');
+
+      console.log('------------------------------');
+      
+
+      /** 
+       * 可选方式：判断 value 存在时再执行操作
+       * 这个方式可以正常修改子组件的值
+       */
+
+      if ( logo.value ) {
+        console.log(logo.value.getAttribute('src'));
+      }
+
+      if ( child.value ) {
+        child.value.isShow = true;
+        console.log(child.value.isShow);
+        child.value.sayHi('use if in onMounted');
+      }
+    })
+
+    return {
+      logo,
+      child
+    }
+    
   }
 });
 </script>
---
+++ src/components/Child.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup () {
+    const isShow = ref<boolean>(false);
+    const sayHi = (source: string): void => {
+      console.log(`Hi —— from ${source}.`);
+    }
+
+    // 需要return出去，父组件才可以拿到
+    return {
+      isShow,
+      sayHi
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
